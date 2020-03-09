651b486b8d0c502887316d206b13f86a0f3f8927
Mateusz Zaremba <mjzaremba1604@gmail.com>
2020-03-09 17:32:53 +0100

    WIP stationary measurements

---
+++ stationary_measurements.py
@@ -0,0 +1,138 @@
+import numpy as np
+import scipy.optimize as opt
+
+import Cit_par as cessna
+import isa
+
+
+def cl_curve(alpha, cn_alpha, alpha_0):
+    return cn_alpha * alpha - cn_alpha * alpha_0
+
+
+def cd_curve(cl, cd_0, e):
+    return cd_0 + np.power(cl, 2) / (np.pi * cessna.A * e)
+
+
+def get_cl_params(alpha, weight, height, true_airspeed):
+    """
+    Inputs should be single values or numpy arrays
+
+    :param alpha: angle of attack
+    :param weight:
+    :param height:
+    :param true_airspeed:
+    :return: cn_alpha, alpha_0
+    """
+
+    temperature = isa.get_t_at_height(height)
+    pressure = isa.get_p_at_temperature(temperature)
+    density = isa.get_rho(pressure, temperature)
+    cn = weight / (0.5 * density * np.power(true_airspeed, 2) * cessna.S)
+
+    popt, _ = opt.curve_fit(cl_curve, alpha, cn)
+    cn_alpha, alpha_0 = popt
+
+    return cn_alpha, alpha_0
+
+
+def get_cd_params(alpha, weight, height, true_airspeed, thrust):
+    """
+    Inputs should be single values or numpy arrays
+
+    :param alpha: angle of attack
+    :param weight:
+    :param height:
+    :param true_airspeed:
+    :param thrust:
+    :return: cd_0, e
+    """
+    temperature = isa.get_t_at_height(height)
+    pressure = isa.get_p_at_temperature(temperature)
+    density = isa.get_rho(pressure, temperature)
+
+    cn_alpha, alpha_0 = get_cl_params(alpha, weight, height, true_airspeed)
+
+    cl = cn_alpha * (alpha - alpha_0)
+    cd = thrust / (0.5 * density * np.power(true_airspeed, 2) * cessna.S)
+
+    popt, _ = opt.curve_fit(cd_curve, cl, cd)
+    cd_0, e = popt
+    return cd_0, e
+
+
+def get_mach_number(height, v_c):
+    """
+
+    :param height:
+    :param v_c: Calibrated airspeed
+    :return: Mach number
+    """
+    pressure = isa.get_p_at_temperature(isa.get_t_at_height(height))
+
+    innermost = np.power(
+        1
+        + (isa.gamma - 1) / (2 * isa.gamma) * (isa.rho_0 / isa.p_0) * np.power(v_c, 2),
+        isa.gamma / (isa.gamma - 1),
+    )
+    middle = np.power(
+        1 + isa.p_0 / pressure * (innermost - 1), (isa.gamma - 1) / isa.gamma
+    )
+    M = np.sqrt(2 / (isa.gamma - 1) * (middle - 1))
+
+    return M
+
+
+def get_true_airspeed(height, measured_temperature, v_c):
+    """
+
+    :param height:
+    :param measured_temperature:
+    :param v_c: Calibrated airspeed
+    :return: True airspeed
+    """
+    M = get_mach_number(height, v_c)
+    temperature = measured_temperature / (1 + (isa.gamma - 1) / 2 * np.power(M, 2))
+    a = isa.get_speed_of_sound(temperature)
+
+    return M * a
+
+def get_reduced_equivalent_airspeed(weight, height, v_t):
+    """
+
+    :param weight:
+    :param height:
+    :param v_t: True airspeed
+    :return: Reduced equivalent airspeed
+    """
+    temperature = isa.get_t_at_height(height)
+    pressure = isa.get_p_at_temperature(temperature)
+    density = isa.get_rho(pressure, temperature)
+
+    v_e = v_t*np.sqrt(density, isa.rho_0)
+    return  v_e*np.sqrt(cessna.W/weight)
+
+def get_cm_derivatives(weight, height, v_t, delta_cg, delta_de, delta_alpha):
+    """
+
+    :param weight:
+    :param height:
+    :param v_t: True airspeed
+    :param delta_cg: Change in CG location
+    :param delta_de: Change in elevator deflection
+    :param delta_alpha: Change in angle of attack
+    :return: cm_alpha, cm_delta
+    """
+    averaged_weight = np.mean(weight)
+    temperature = isa.get_t_at_height(height)
+    pressure = isa.get_p_at_temperature(temperature)
+    density = isa.get_rho(pressure, temperature)
+    averaged_density = np.mean(density)
+    averaged_v_t = np.mean(v_t)
+
+    cm_delta = -1/delta_de*averaged_weight/(0.5*averaged_density*np.power(averaged_v_t,2)*cessna.S)*delta_cg/cessna.c
+    cm_alpha = -delta_de/delta_alpha*cm_delta
+
+    return cm_alpha, cm_delta
+
+
+
